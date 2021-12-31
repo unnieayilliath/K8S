7b7bfc92fbd9b8b7b9cac00ebef6f8787497d6a4
unnie <unnie.ayilliath@gmail.com>
2021-12-31 21:18:28 +0000

    set base structure

---
+++ k8scontroller.py
@@ -0,0 +1,86 @@
+from consolehelper import ConsoleHelper
+from navigationcontroller import NavigationController
+import os
+ # -------------------------------------------------------------------------------------------------
+ # A class for controlling interactions with the K8S
+class K8SController:
+   
+    # --------------------------------------------------------------------------------------------------
+    # This is the main method for this controller
+    def load_screen(self):
+        loop=True
+        while loop:
+            ConsoleHelper.clear()
+            self.__list_pods()
+            loop=self.__perform_action()
+    # -------------------------------------------------------------------------------------------------
+    # This protected method returns all containers
+    def __list_pods(self):
+       os.system("ls")
+    # ------------------------------------------------------------------------------------------------
+    # This method allows user to select an action
+    def __perform_action(self):
+        repeat = True
+        # show all actions for this menu
+        selectedAction = NavigationController.print_actions()
+        if selectedAction is None:
+            repeat=False
+        else:
+            # add the page to the breadcrumb
+            ConsoleHelper.append_breadcrumb(selectedAction["name"])
+        try:
+            match selectedAction["key"]:
+                case "describe":
+                    self.__describe_pod()
+                case "create":
+                    self.__create_deployment()
+                case "update":
+                    self.__update_deployment()
+                case "deploy":
+                    self.__deploy_ubuntu()
+                case "delete":
+                    self.__delete_deployment()
+        except Exception as ex:
+            ConsoleHelper.print_error(ex)
+            print("Something went wrong! Please check error details above")
+        if repeat:
+            input("Press any key to continue..")
+            # remove the last added item from breadcrumb
+            ConsoleHelper.pop_breadcrumb()
+        return repeat
+    # ------------------------------------------------------------------------------------------------------------
+    # This method describes a pod
+    def __describe_pod(self):
+        podName=ConsoleHelper.get_alphanumeric_input("Please enter the pod name:\t")
+        os.system("ls")
+    # ------------------------------------------------------------------------------------------------------------
+    # This method creates a deployment with 2 pods.
+    def __create_deployment(self):
+        podName=ConsoleHelper.get_alphanumeric_input("Please enter the pod name:\t")
+        image=ConsoleHelper.get_number_input(1,2,"Please select image,\n 1. nginx:1.20\t2. couchbase:6.0.5\n > ")
+        os.system("ls")
+    # ------------------------------------------------------------------------------------------------------------
+    # This method updates a deployment
+    def __update_deployment(self):
+        podName=ConsoleHelper.get_alphanumeric_input("Please enter the pod name:\t")
+        os.system("ls")
+    # ------------------------------------------------------------------------------------------------------------
+    # This method deploys an ubuntu pod
+    def __deploy_ubuntu(self):
+        podName=ConsoleHelper.get_alphanumeric_input("Please enter the pod name:\t")
+        os.system("ls")
+    # ------------------------------------------------------------------------------------------------------------
+    # This method deploys an ubuntu pod
+    def __delete_deployment(self):
+        deploymentName=ConsoleHelper.get_alphanumeric_input("Please enter the deployment to delete:\t")
+        os.system("ls")
+    # ------------------------------------------------------------------------------------------------------------
+    # This method runs a predfined scenario
+    def __run_predfined_scenario(self):
+        deploymentName=ConsoleHelper.get_alphanumeric_input("Please enter the deployment to delete:\t")
+        os.system("ls")
+  
+  
+   
+        
+
---
+++ navigationcontroller.py
@@ -0,0 +1,77 @@
+########################################
+#     This is the file 'navigation.py'    #
+########################################
+import json
+from consolehelper import ConsoleHelper
+class NavigationController:
+    # -------------------------------------------------------------------------------------------------
+    # A class for showing all the navigation options. This class has methods which will read the menu.json file and display the menu structure
+   def __init__(self):
+       # load menu from json file
+        self.__load_menu_json()
+        NavigationController._selectedTopMenu=None
+    # ------------------------------------------------------
+    # This method loads the menu data from the json file.
+   def __load_menu_json(self):
+       # Opening menu JSON file
+       menuFile = open('menu.json')
+       # returns JSON object as # a dictionary
+       NavigationController._menuData = json.load(menuFile)
+       # Closing file
+       menuFile.close()
+   # ------------------------------------------------------
+   # This method displays menu based on logged in user's role and retrieves user input
+   def display_menu(self):
+       # show level 1 menu
+       ConsoleHelper._breadcrumb=["Home"]
+         # if nothing is selected on top menu then show top menu and ask for selection
+       NavigationController._selectedTopMenu=self.__print_menu(NavigationController._menuData,1);
+       ConsoleHelper.append_breadcrumb(NavigationController._selectedTopMenu['name']);
+       # return the selected topmmenu
+       return NavigationController._selectedTopMenu
+    # ------------------------------------------------------
+   # This method prints the menu and returns user selection
+   def __print_menu(self,menu,level):
+        ConsoleHelper.clear();
+        filteredMenu=menu
+        index=0
+        for menuItem in filteredMenu:
+            index+=1
+            print(f"{index}. {menuItem['name']}")
+        selectedIndex= ConsoleHelper.get_number_input(0,index,"Please enter your selection \t");
+        if selectedIndex==0:
+            # user pressed back button, so reload the top menu
+            if level==2:
+              # user wants to re load top menu
+              self._selectedTopMenu=None;
+        else:
+         # get the menu item which was selected by user
+         selectedMenuItem=filteredMenu[selectedIndex-1]
+         return selectedMenuItem;
+   # ------------------------------------------------------
+   # This method prints all actions available under current selected menu
+   @staticmethod
+   def print_actions(selectedMenu=None):
+       currentSelectedMenu=NavigationController._selectedTopMenu if selectedMenu is None else selectedMenu
+       if currentSelectedMenu is None:
+           ConsoleHelper.print_warning("No actions available!")
+       else:
+           # get the actions belonging to top menu
+           actions=list(currentSelectedMenu["actions"])
+           index=0
+           actionString="Please choose an action\n"
+           for action in actions:
+               index+=1
+               actionString+=f"{index}. {action['name']}\t"
+           selectedIndex=ConsoleHelper.get_number_input(0,len(actions),f"{actionString}\n")
+           if selectedIndex==0:
+               return None
+           else:
+               selectAction=actions[selectedIndex-1]
+               return selectAction
+               
+               
+       
+       
+       
+   
